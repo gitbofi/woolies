7d52e09e48d55a871ba53c65a4fe4b09bfb4b4f1
Bofi <84191696+gitbofi@users.noreply.github.com>
2025-01-10 11:48:48 +0100

    changes changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
-import { heroVideo } from '../utils'
-import CTA from './CTA'
-import GreenBanner from './GreenBanner'
+import React from "react";
+import { heroVideo } from "../utils";
+import CTA from "./CTA";
+import GreenBanner from "./GreenBanner";
 
 const Hero = () => {
   return (
     <>
-      <section>
-        <video src={heroVideo} autoPlay loop muted className='relative h-heroLarge object-cover xl:w-full'></video>
-        <div id="text-wrapper" className="absolute top-1/3 left-0 right-0 text-light flex flex-col items-center text-center">
+      <section className="z-10">
+        <video
+          src={heroVideo}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="relative h-heroLarge object-cover xl:w-full"
+        ></video>
+        <div
+          id="text-wrapper"
+          className="absolute top-1/3 left-0 right-0 text-light flex flex-col items-center text-center"
+        >
           <h1 className="text-6xl md:text-8xl font-artifika">woolies</h1>
-          <h3 className='text-md uppercase tracking-widest font-now'>Fall Collection</h3>
-          <p className='text-md md:text-sm mt-8 w-80'>Für eine bessere Zukunft. Entdecke unsere neue Herbst Kollektion mit neuen Pieces aus nachhaltig produzierter Schafwolle.</p>
-          <CTA/>
+          <h3 className="text-md uppercase tracking-widest font-now">
+            Fall Collection
+          </h3>
+          <p className="text-md md:text-sm mt-8 w-80">
+            Für eine bessere Zukunft. Entdecke unsere neue Herbst Kollektion mit
+            neuen Pieces aus nachhaltig produzierter Schafwolle.
+          </p>
+          <CTA />
         </div>
-        <GreenBanner/>
+        <GreenBanner />
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const MobileMenu = () => {
   return (
     <>
-      <nav className="w-screen lg:hidden bg-dark text-light font-now uppercase flex flex-col tracking-widest p-6 pb-12 space-y-4 text-right">
-        <a href="#" className="">Oberteile</a>
+      <nav className="w-screen absolute z-30 right-0 top-16 lg:hidden bg-black text-light font-now uppercase flex flex-col tracking-widest p-6 pb-12 space-y-4 text-right">
+        <a href="#" className="">
+          Oberteile
+        </a>
         <a href="#">Hosen</a>
         <a href="#">Socken</a>
         <a href="#">Accessoires</a>
-        <a href="#" className="pt-8">Nachhaltigkeit</a>
+        <a href="#" className="pt-8">
+          Nachhaltigkeit
+        </a>
         <a href="#">Über Woolies</a>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default MobileMenu
+export default MobileMenu;
