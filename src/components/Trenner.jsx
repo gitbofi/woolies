caf8bdc2562f8d563f8ac821f4d0022051e11ef1
Bofi <84191696+gitbofi@users.noreply.github.com>
2025-01-10 09:56:46 +0100

    bug fixes

--- src/components/Trenner.jsx
+++ src/components/Trenner.jsx
@@ -3,7 +3,7 @@ import { star } from "../utils";
 
 const Trenner = () => {
   return (
-    <div className="flex flex-col items-center lg:flex lg:flex-row text-center lg:justify-between w-5/6 justify-self-center my-12 xl:mt-0 py-4 lg:border-y-2 border-zinc-800 max-w-7xl">
+    <div className="flex flex-col items-center lg:flex lg:flex-row text-center lg:justify-between my-12 xl:mt-0 py-4 lg:border-y-2 border-zinc-800 max-w-7xl mx-16">
       <div>
         <img src={star} alt="" className="animate-slowspin" />
       </div>
--- src/components/Versprechen.jsx
+++ src/components/Versprechen.jsx
@@ -9,7 +9,7 @@ const Versprechen = () => {
         <h2>Unser Versprechen an die Natur</h2>
         <div
           id="content-wrapper"
-          className="lg:flex w-full px-8 max-w-7xl pt-8 pb-24 lg:pb-0"
+          className="lg:flex w-full px-8 lg:px-16 max-w-7xl pt-8 pb-24 lg:pb-0"
         >
           <div>
             <p className="pt-4 pb-4 text-center lg:text-left">
